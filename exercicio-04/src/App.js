0c98967dbf22438927a61b00f0c37ff2639fda69
MayllaRabay <mayllarabay@gmail.com>
2021-06-06 14:35:45 -0300

    Componente 'CardInfo' criado.

--- exercicio-04/src/App.js
+++ exercicio-04/src/App.js
@@ -4,6 +4,40 @@ import idea from './assets/idea.svg';
 import search from './assets/search.svg';
 import software from './assets/software.svg';
 
+const cards = [
+  {
+    className:"card card--1",
+    title: "Supervisor",
+    paragraph: "Monitors activity to identify project roadblocks",
+    image: search
+  }, {
+    className:"card card--2",
+    title: "Team Builder",
+    paragraph: "Scans our talent network to create the optimal team for your project",
+    image: homepage
+  }, {
+    className:"card card--3",
+    title: "Karma",
+    paragraph: "Regularly evaluates our talent to ensure quality",
+    image: idea
+  }, {
+    className:"card card--4",
+    title: "Calculator",
+    paragraph: "Uses data from past projects to provide better delivery estimates",
+    image: software
+  }
+];
+
+function CardInfo({ card }) {
+  return (
+    <article className={card.className}>
+      <div className="card__title">{card.title}</div>
+      <div className="card__paragraph">{card.paragraph}</div>
+      <img src={card.image} alt="search icon" class="card__image" />
+    </article>
+  );
+}
+
 function App() {
   return (
     <>
@@ -14,31 +48,12 @@ function App() {
       </div>
 
       <div className="main-container">
-        <article className="card card--1">
-          <div className="card__title">Supervisor</div>
-          <div className="card__paragraph">Monitors activity to identify project roadblocks</div>
-          <img src={search} alt="search icon" class="card__image" />
-        </article>
-
+        {<CardInfo card = {cards[0]} />}
         <div className="middle-container">
-          <article className="card card--2">
-            <div className="card__title">Team Builder</div>
-            <div className="card__paragraph">Scans our talent network to create the optimal team for your project</div>
-            <img src={homepage} alt="homepage icon" class="card__image" />
-          </article>
-
-          <article className="card card--3">
-            <div className="card__title">Karma</div>
-            <div className="card__paragraph">Regularly evaluates our talent to ensure quality</div>
-            <img src={idea} alt="light bulb icon" class="card__image" />
-          </article>
+          {<CardInfo card = {cards[1]} />}
+          {<CardInfo card = {cards[2]} />}
         </div>
-
-        <article className="card card--4">
-          <div className="card__title">Calculator</div>
-          <div className="card__paragraph">Uses data from past projects to provide better delivery estimates</div>
-          <img src={software} alt="desktop icon" class="card__image" />
-        </article>
+        {<CardInfo card = {cards[3]} />}
       </div>
     </>
   );
