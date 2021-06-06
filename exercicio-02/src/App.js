ac55ffa3503e63ca5f524f7700d8124a7674ee13
MayllaRabay <mayllarabay@gmail.com>
2021-06-05 21:26:46 -0300

    HTML, CSS e imagens importados para App.js, App.css e assets.

--- exercicio-02/src/App.js
+++ exercicio-02/src/App.js
@@ -1,8 +1,39 @@
 import './App.css';
+import luxury from './assets/icon-luxury.svg';
+import sedans from './assets/icon-sedans.svg';
+import suvs from './assets/icon-suvs.svg';
 
 function App() {
   return (
-    <h1>Exercício 2</h1>
+    <section className="cards">
+      <div className="card card--1">
+        <img src={sedans} alt="sedans" />
+        <h1>sedans</h1>
+        <p>
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
+        </p>
+        <a href="/" className="btn">Learn More</a>
+      </div>
+      <div className="card card--2">
+        <img src={suvs} alt="suvs" />
+        <h1>suvs</h1>
+        <p>
+          Take an SUV for its spacious interior, power, and versatility. Perfect
+          for your next family vacation and off-road adventures.
+        </p>
+        <a href="/" className="btn">Learn More</a>
+      </div>
+      <div className="card card--3">
+        <img src={luxury} alt="luxury" />
+        <h1>luxury</h1>
+        <p>
+          Cruise in the best car brands without the bloated prices. Enjoy the
+          enhanced comfort of a luxury rental and arrive in style.
+        </p>
+        <a href="/" className="btn">Learn More</a>
+      </div>
+    </section>
   );
 }
 
