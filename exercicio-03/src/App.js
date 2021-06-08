e47154e8154353c7b200254f61c54cafcfcbdb0a
MayllaRabay <mayllarabay@gmail.com>
2021-06-08 18:10:37 -0300

    Component 'JobCard' criado.

--- exercicio-03/src/App.js
+++ exercicio-03/src/App.js
@@ -4,6 +4,44 @@ import photosnap from './assets/photosnap.svg';
 import shapeLeft from './assets/shape-left.svg';
 import shapeRight from './assets/shape-right.svg';
 
+const jobs = [
+  {
+    companyImage: photosnap,
+    companyName: "Photosnap",
+    newJob: true,
+    jobName: "Senior Frontend Developer",
+    jobTime: "1d ago",
+    jobContract: "Full Time",
+    jobLocation: "USA only",
+    jobTechs: ["Frontend", "Senior", "HTML", "CSS", "Javascript"]
+  }
+];
+
+function JobCard({ job }) {
+  return (
+    <div className="card">
+          <img src={job.companyImage} alt="" className="card__company-image" />
+          <div className="card__info-wrapper">
+            <div>
+              <div className="info__company-name">{job.companyName}</div>
+              {job.newJob ? <div className="info__new-job-alert" >NEW!</div> : ""}
+            </div>
+            <div className="info__job-name">{job.jobName}</div>
+            <div className="info__job-info">
+              <div className="job__time">{job.jobTime}</div>
+              <img src={dot} alt="" />
+              <div className="job__contract">{job.jobContract}</div>
+              <img src={dot} alt="" />
+              <div className="job__location">{job.jobLocation}</div>
+            </div>
+          </div>
+          <div className="card__tech-wrapper">
+            {job.jobTechs.map( jobTech => <div className="card__tech-name">{jobTech}</div>)}
+          </div>
+        </div>
+  );
+}
+
 function App() {
   return (
     <>
@@ -13,31 +51,7 @@ function App() {
       </div>
 
       <div className="main-content-wrapper">
-        <div className="card">
-          <img src={photosnap} alt="" className="card__company-image" />
-          <div className="card__info-wrapper">
-            <div>
-              <div className="info__company-name">Photosnap</div>
-              <div className="info__new-job-alert">NEW!</div>
-            </div>
-            <div className="info__job-name">Senior Frontend Developer</div>
-            <div className="info__job-info">
-              <div className="job__time">1d ago</div>
-              <img src={dot} alt="" />
-              <div className="job__contract">Full Time</div>
-              <img src={dot} alt="" />
-              <div className="job__location">USA only</div>
-            </div>
-          </div>
-          
-          <div className="card__tech-wrapper">
-            <div className="card__tech-name">Frontend</div>
-            <div className="card__tech-name">Senior</div>
-            <div className="card__tech-name">HTML</div>
-            <div className="card__tech-name">CSS</div>
-            <div className="card__tech-name">Javascript</div>
-          </div>
-        </div>
+        {jobs.map(job => <JobCard job = {job} />)}
       </div>
     </>
   );
